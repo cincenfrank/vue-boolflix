cad1858609a0c3952ba27a910610fcaa07ec02bc
cincenfrank <fpetrangeli1@gmail.com>
2021-11-19 15:23:23 +0100

    added font-awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 // import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
